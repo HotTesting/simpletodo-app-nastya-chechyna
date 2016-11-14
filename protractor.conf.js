69ea1b63241b5ad48e602f0314cf01f381cf58a8
nastya-chechyna <nastya.chechyna@gmail.com>
2016-11-14 14:34:56 +0200

    delete_notes_test
    delete_notes_test

--- protractor.conf.js
+++ protractor.conf.js
@@ -2,4 +2,5 @@ module.exports.config = {
     useAllAngular2AppRoots: true,
     specs: 'spec.js',
     directConnect: true
+
 }
--- spec.js
+++ spec.js
@@ -1,7 +1,52 @@
+let URL = 'http://maxcabrera.com/code/todo-list/'
 describe('suite', function () {
 
     it('test', function () {
-        browser.get('http://maxcabrera.com/code/todo-list/')
+        browser.get(URL)
         expect(browser.getTitle()).toBe('Angular2 Seed')
     })
+
+    it('should create new note', function () {
+        browser.get(URL)
+        let webelement = $('input.enter-todo')
+   
+        webelement.sendKeys('T')
+        browser.sleep(2000)
+        webelement.submit()
+
+        let notes = $$('todo-list .small-12')
+        expect(notes.getText()).toContain('T')
+        //expect(notes.getText()).not.toContain('New Shiny note')
+       
+    })
+
+    it('Should delete one note', function() {
+        browser.get(URL)
+        let countBefore = element.all(by.css('todo-list .small-12'))
+
+        element($("input[type='checkbox']").click())
+        let countAfter = element.all(by.css('todo-list .small-12'))
+        browser.sleep(2000)
+        //expect(countBefore.count().not.toEqual(countAfter.count()))
+        expect(countAfter.count() === countBefore.count()).toBe(false)
+    }
+    )
+
+    it ('should delete all notes', function () {
+	browser.get(URL)
+    element.all(by.css(".todo-container input[type='checkbox']")).then(function(defNotes) {
+   
+        for (let i = 0; i < defNotes.length; i++) {
+           defNotes[i].click();
+        }
+    
+    });
+    let countAfter = element.all(by.css('todo-list .small-12'))
+        //expect(countBefore.count().not.toEqual(countAfter.count()))
+     expect(countAfter.count()).toBe(0);
+    
+
 })
+})
+
+// baseUrl?: string;
